2ce1cfd458197276b20d08a053521c2d28ab6744
William Lee <william93lee@gmail.com>
2018-04-01 21:53:01 -1000

    Add scss plugins

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'WillAndElizabeth',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+};
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import Navbar from '../components/Navbar';
+import './index.scss';
 
 export default class TemplateWrapper extends React.Component {
   static propTypes = {
