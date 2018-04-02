d6ccc2ecb787e8298a42dddcadb6537788d3c832
William Lee <william93lee@gmail.com>
2018-04-01 22:15:16 -1000

    Add npprogres

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'WillAndElizabeth',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: '#f09a7a',
+        showSpinner: false,
+      },
+    },
+  ],
 };
