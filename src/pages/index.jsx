be3fbb4b622150036f1e52b269777bf63ae5c312
William Lee <william93lee@gmail.com>
2018-04-02 18:16:21 -1000

    Remove h tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,12 +9,11 @@ export default () => (
       src="https://s3.amazonaws.com/wewedding/IMG_5874.JPG"
       alt=""
     />
-    <h3 className="h3 h3--lighter h3--uppercase">Come Celebrate Our Wedding</h3>
     <h1 className="h1">William Lee</h1>
     <h3 className="h3 h3--lighter h3--italic">
       -&nbsp;<em>and</em>&nbsp;-
     </h3>
     <h1 className="h1">Elizabeth Chun</h1>
-    <h3 className="h3 h3--lighter h3--uppercase">Saturday, October 6, 2018</h3>
+    <h3 className="h3 h3--uppercase">Saturday, October 6, 2018</h3>
   </section>
 );
