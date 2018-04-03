085580b0b3f3366d1c6f31c8024e8f6fcba6b359
William Lee <william93lee@gmail.com>
2018-04-02 16:48:08 -1000

    Add img

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,17 @@ import Script from 'react-load-script';
 
 export default () => (
   <section>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-    <h1>Come Celebrate Our Wedding</h1>
+    <img
+      className="img"
+      src="https://s3.amazonaws.com/wewedding/IMG_5874.JPG"
+      alt=""
+    />
+    <h3 className="h3 h3--lighter h3--uppercase">Come Celebrate Our Wedding</h3>
+    <h1 className="h1">William Lee</h1>
+    <h3 className="h3 h3--lighter h3--italic">
+      -&nbsp;<em>and</em>&nbsp;-
+    </h3>
+    <h1 className="h1">Elizabeth Chun</h1>
+    <h3 className="h3 h3--lighter h3--uppercase">Saturday, October 6, 2018</h3>
   </section>
 );
