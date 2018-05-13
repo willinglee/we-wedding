ea1473be0d2a006fdc6c7057dd1644c030520977
William Lee <william93lee@gmail.com>
2018-05-13 08:12:30 -0700

    Make penny photo smaller 🐶. Add wedding time. 👰

--- src/pages/registry.jsx
+++ src/pages/registry.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default () => (
-  <section className="main">
+  <section
+    className="main"
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
     <img
       className="img"
       src="https://d2gw80r1s17zo1.cloudfront.net/penny.png"
       alt=""
+      style={{ maxWidth: '80%' }}
     />
     <h3 className="h3" style={{ width: '55%', margin: '40px auto' }}>
       We need nothing more than your loving company. But if you must, monetary
--- src/pages/wedding.jsx
+++ src/pages/wedding.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 export default () => (
   <section className="main">
-    <h3 style={{ marginTop: '50px' }} className="h3 h3--lighter">
-      The wedding will be held on Saturday, October 5th, 2018. We'll start with
+    <h3
+      style={{ marginTop: '50px', width: '55%', margin: '0 auto' }}
+      className="h3 h3--lighter"
+    >
+      The wedding will be held on Saturday, October 6th, 2018. We'll start with
       a ceremony, then move to the church reception hall. Dress code will be
       semi-formal attire.
+    </h3>
+    <div className="separator">
+      <hr />
+    </div>
+    <h3 className="h3" style={{ width: '55%', margin: '50px auto 0 auto' }}>
+      Wedding Ceremony & Reception
+    </h3>
+    <h3 className="h3 h3--lighter" style={{ width: '55%', margin: '0 auto' }}>
+      3:30PM
     </h3>
   </section>
 );
