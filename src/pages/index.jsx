5a5eb221e2bb8d9b35b7fe90630ebc7f5bed87cf
William Lee <william93lee@gmail.com>
2018-05-12 22:45:24 -0700

    Fix styles in the home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,9 +9,15 @@ export default () => (
       src="https://d2gw80r1s17zo1.cloudfront.net/IMG_5874.JPG"
       alt=""
     />
+    <h3 style={{ marginTop: '50px' }} className="h3 h3--uppercase h3--lighter">
+      Come Celebrate Our Wedding
+    </h3>
+    <div className="separator">
+      <hr />
+    </div>
     <div className="content">
       <h1 className="h1">William Lee</h1>
-      <h3 className="h3 h3--lighter h3--italic">
+      <h3 className="h3 h3--lighter h3--italic h3--cursive">
         -&nbsp;<em>and</em>&nbsp;-
       </h3>
       <h1 className="h1">Elizabeth Chun</h1>
@@ -20,5 +26,9 @@ export default () => (
       <hr />
     </div>
     <h3 className="h3 h3--uppercase h3--lighter">Saturday, October 6, 2018</h3>
+    <h3 className="h3" style={{ width: '55%', margin: '100px auto' }}>
+      William and Elizabeth joyfully request the pleasure of your company as we
+      speak our vows and join in marriage in the presence of family and friends.
+    </h3>
   </section>
 );
