a8044e9b6cd20dee3100a8cd3d1fa9d31b10d209
William Lee <william93lee@gmail.com>
2018-08-18 15:45:39 -0700

    Add photos. Delete photos

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Script from 'react-load-script';
-import mainImage from './images/liz_will_engagement_0122.jpg';
+import mainImage from './liz_will_engagement_0122.jpg';
 
 export default () => (
   <section className="main">
